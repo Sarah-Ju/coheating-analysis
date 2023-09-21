55b5deab4af373935ddbfc57476837093f4363b7
Sarah_JURICIC <sarah.juricic@cstb.fr>
2023-09-21 17:13:57 +0200

    add structure to create automatic plots

---
+++ coheating/plot_utils.py
@@ -0,0 +1,23 @@
+import seaborn as sns
+
+def plot_siviour(ph_on_temp_diff, isol_on_temp_diff):
+    """ plot a Siviour plot to help analyse the data
+
+    todo write function
+
+    """
+    return
+
+
+def plot_multilin_reg(ph, isol, text):
+    """ plot a scatter-plot of the co-heating data with two exogeneous variables
+
+    x-axis is outdoor temperature
+    y-axis is heating power
+    color is solar radiation
+
+    todo write function
+
+    """
+
+    return
